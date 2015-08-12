1236512b7b69fb2021b637e85aa5c6a6d094396f
gggeek <ggiunta@kaliop.com>
2015-08-12 23:16:45 +0100

    Fix the calculation of the timing filter

--- Services/MessageConsumer/EventListener/TimingFilter.php
+++ Services/MessageConsumer/EventListener/TimingFilter.php
@@ -5,8 +5,10 @@ namespace Kaliop\QueueingBundle\Services\MessageConsumer\EventListener;
 use Kaliop\QueueingBundle\Events\MessageReceivedEvent;
 
 /**
- * A class which can be registered as listener in order to time the speed of message consumption:
- * it will display the passed time every N received messages
+ * A class which can be registered as listener in order to measure the speed of message consumption:
+ * it will display every N received messages the time taken as well as the messages/second.
+ * This kind of measure makes most sense when the queue is full of messages, or at least the message producer
+ * is running at its full capacity.
  *
  * @todo inject the Sf logger and use it for output instead of plain echo?
  */
@@ -17,23 +19,27 @@ class TimingFilter
     protected $started = 0;
 
     /**
-     * @param int $count the number of received messages to measure the time for
+     * @param int $count the number of received messages to measure the time for. Should be >= 1
      */
     public function __construct($count)
     {
         $this->count = $count;
     }
 
+    /**
+     * NB: we measure the time consumed for processing of N messages upon receiving the Nth+1 message.
+     * This because we have only the before-execution event available.
+     */
     public function onMessageReceived(MessageReceivedEvent $event)
     {
         if ($this->received === 0) {
             $this->started = microtime(true);
         }
-        $this->received++;
-        if ($this->received === $this->count) {
+        else if ($this->received === $this->count) {
             $elapsed = microtime(true) - $this->started;
             printf("Time spent to receive {$this->count} messages: %.3f secs (%.3f m/s)\n", $elapsed, $this->count / $elapsed );
             $this->received = 0;
         }
+        $this->received++;
     }
 }
