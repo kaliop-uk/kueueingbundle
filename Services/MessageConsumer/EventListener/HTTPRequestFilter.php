8f02a76edf5c0f185ca4aa6cd39a29ef43518fa1
gggeek <ggiunta@kaliop.com>
2015-08-16 00:29:54 +0100

    Add message producer+consumer to execute http calls

---
+++ Services/MessageConsumer/EventListener/HTTPRequestFilter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Kaliop\QueueingBundle\Services\MessageConsumer\EventListener;
+
+use Kaliop\QueueingBundle\Events\MessageReceivedEvent;
+
+/**
+ * A class which can be registered as listener in order to filter SymfonyService messages
+ */
+class HTTPRequestFilter
+{
+    protected $allowedServers;
+
+    public function __construct( array $allowedServers )
+    {
+        $this->allowedServers = $allowedServers;
+    }
+
+    public function onMessageReceived(MessageReceivedEvent $event)
+    {
+        // filter out unwanted events
+        if (! $event->getConsumer() instanceof \Kaliop\QueueingBundle\Services\MessageConsumer\HTTPRequest)
+            return;
+
+        $body = $event->getBody();
+        $url = @$body['url'];
+        $options = @$body['options'];
+        if (empty($options)) {
+            /// we leave it up to the consumer to respond to these messages...
+            return;
+        }
+
+        if (!$this->isServiceAllowed($url, $options)) {
+            $event->stopPropagation();
+        }
+    }
+
+    protected function isServiceAllowed($url, $options) {
+
+        $server = parse_url($url, PHP_URL_HOST);
+
+        foreach($this->allowedServers as $allowedServer) {
+
+            if (substr($allowedServer, 0, 7) === 'regexp:') {
+                if (preg_match(substr($allowedServer, 7), $url)) {
+                    return true;
+                }
+            }
+            elseif ($allowedServer == $server) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Services/MessageConsumer/HTTPRequest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Kaliop\QueueingBundle\Services\MessageConsumer;
+
+use Kaliop\QueueingBundle\Services\MessageConsumer;
+use Kaliop\QueueingBundle\Command\ConsumerCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Process\Process;
+
+/**
+ * This service can be registered to consume "send http requests" messages
+ */
+class HTTPRequest extends MessageConsumer
+{
+    /**
+     * @param array $body
+     * @throws \UnexpectedValueException
+     */
+    public function consume( $body )
+    {
+        // validate members in $body
+        if (
+            !is_array( $body ) ||
+            empty( $body['url'] ) ||
+            ( isset( $body['options'] ) && !is_array( $body['options'] ) )
+        )
+        {
+            throw new \UnexpectedValueException( "Message format unsupported: missing 'url'? Received: " . json_encode( $body ) );
+        }
+
+        if (!isset($body['options'])) {
+            $body['options'] = array();
+        }
+
+        $this->executeRequest( $body['url'], $body['options'] );
+    }
+
+    /**
+     * Runs an sf command as a separate php process - this way we insure the worker is stable (no memleaks or crashes)
+     *
+     * @param string $url
+     * @param array $options
+     * @return string|false
+     */
+    protected function executeRequest( $url, array $options = array() )
+    {
+        $ch = curl_init($url);
+
+        foreach($options as $name => $value) {
+            if (!curl_setopt($ch, $name, $value)) {
+                if( $this->logger )
+                {
+                    $this->logger->warning( "Option could not be set to Curl: $name, value: $value" );
+                }
+            }
+        }
+
+        // some options we have to force
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $results = curl_exec($ch);
+        if ($results === false) {
+            if( $this->logger )
+            {
+                $this->logger->error( "Curl request failed: " . curl_error($ch));
+            }
+        }
+
+        // close cURL resource, and free up system resources
+        curl_close($ch);
+
+        return $results;
+    }
+}
